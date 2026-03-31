cc9408a8069c4b98adf10da3cee65e758e4c344e
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-03-31 17:24:23 +0400

    project init continue

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Main from './Main'
+
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  )
+}
+export default App
---
+++ src/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div>default</div>
+}
+
+export default Main
