6f9970e7ccbb3a1388f15bc23af6033f025856e3
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-04-01 14:38:58 +0400

    added hot fix and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import EditProfilePage from './pages/EditProfilePage'
 import MainPage from './pages/MainPage'
 
 const App = () => {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainPage />} path="/" />
+        <Route element={<EditProfilePage />} path="/edit/:id" />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
---
+++ src/pages/EditProfilePage.tsx
@@ -0,0 +1,5 @@
+const EditProfilePage = () => {
+  return <div>Profile Page</div>
+}
+
+export default EditProfilePage
