6f4dc64ee90bca8b9a2eb53fae1c1924ee9719c4
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-04-01 17:02:39 +0400

    added users store and archive func

---
+++ src/store/UsersStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+import type { User } from '../entities/users/types'
+
+interface ArchivedUser extends User {
+  id: number
+}
+
+interface storeState {
+  users: User[] | null
+  archivedUsers: ArchivedUser[]
+  archiveUser: (userId: number) => void
+}
+
+export const useUserStore = create<storeState>((set) => ({
+  users: null,
+  archivedUsers: [],
+
+  archiveUser: (userId: number) =>
+    set((state) => {
+      const archivedUser = state.users?.find((user) => user.id === userId)
+      console.log('Пользователь добавлен в архив', archivedUser)
+
+      return {
+        archivedUsers: [
+          ...state.archivedUsers,
+          { ...archivedUser } as ArchivedUser
+        ]
+      }
+    })
+}))
