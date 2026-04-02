623572b0fef7ded662de06fb5414a0bb32df473a
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-04-02 20:32:02 +0400

    added styles for usercard

--- src/components/main-page/user-card/UserCard.tsx
+++ src/components/main-page/user-card/UserCard.tsx
@@ -4,18 +4,22 @@ import styles from './UserCard.module.scss'
 const UserCard = ({ user, archiveUser, activateUser, hideUser }) => {
   return (
     <div className="card">
-      <div className={styles.avatar}>
-        <img alt="avatar" src="/photo-example.png" />
+      <div className={styles.container}>
+        <div className={styles.avatar}>
+          <img alt="avatar" src="/photo-example.png" />
+        </div>
+        <div className={styles.userData}>
+          <div>{user.name}</div>
+          <div>{user.companyName}</div>
+          <div>{user.city}</div>
+        </div>
+        <DropdownMenu
+          activateUser={activateUser}
+          archiveUser={archiveUser}
+          hideUser={hideUser}
+          userId={user.id}
+        />
       </div>
-      <div>{user.name}</div>
-      <div>{user.companyName}</div>
-      <div>{user.city}</div>
-      <DropdownMenu
-        activateUser={activateUser}
-        archiveUser={archiveUser}
-        hideUser={hideUser}
-        userId={user.id}
-      />
     </div>
   )
 }
