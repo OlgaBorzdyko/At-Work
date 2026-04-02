54d4510036c828ca4c13a599f819d56691bac581
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-04-02 19:55:21 +0400

    added topbar elements

--- src/components/layouts/topbar/TopBar.tsx
+++ src/components/layouts/topbar/TopBar.tsx
@@ -1,5 +1,29 @@
 import styles from './topbar.module.scss'
 const TopBar = () => {
-  return <div className={styles.topBar}>тут будет топ бар</div>
+  return (
+    <header className={styles.topBar}>
+      <div className={styles.left}>
+        <a className={styles.logo} href="/">
+          <img alt="At-work logo" src="/logo-sign.png" />
+          <span>at-work</span>
+        </a>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.notification}>
+          <button>
+            <img alt="likes" src="/like.png" />
+          </button>
+          <button>
+            <img alt="notifications" src="/bell.png" />
+          </button>
+        </div>
+
+        <div className={styles.profile}>
+          <img alt="avatar" src="/photo-example.png" />
+          <span className={styles.userName}>Ivan1234</span>
+        </div>
+      </div>
+    </header>
+  )
 }
 export default TopBar
